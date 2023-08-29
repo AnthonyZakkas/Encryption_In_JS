9d22887280183ac6066d10e28005074b1c4a8d67
Anthony <81086656+AnthonyZakkas@users.noreply.github.com>
2023-08-29 18:25:22 +0300

    Update main.js

--- Finished Project/main.js
+++ Finished Project/main.js
@@ -1,4 +1,4 @@
-let input = `okay re trele m na poume twra gamw`;
+let input = `this be very crazy`;
 const alphabets = `ϋwDC#m%ŒËΙĂέ]èπύΰ,OKäœξWήΊ[AΉÿț-η@ιù_GΜΛΡVώLìυ(oβXÎâdÂă7ϊȚΔHΣ*JuaΝZΟíE.fÇb0ÖόITPj!"φΦ{ΧχοδνS2FΘYΒî;$εû)öΗÏrΓΞ+ÊȘêκpÉÙàάΎúΆΩÈ/ΖΨevΐ€ß Æσ1Ä5MÛçxΈΚhω48znó}γρéẞ^ΠșζΫB'Uæô63kςλitΑΌψüÜQlgÀ9qτÔyΏΥR&òë=N:θcίΤΕΪsïμα`;
 let key1 = "asdiohasduiosghduasdgh";
 let key2 = "sjkdbhasjkudfasio";
@@ -10,8 +10,8 @@ const Decryption = require("./decryption");
 Encryption.EncryptRK(input,null,alphabets);
 Encryption.EncryptSK(input,key1,key2,alphabets);
 
-Decryption.DecryptRK(`ΘόO€ιÎàÛöîÈ'ÿUD}%ή=KjΎFúΰ=Bά"4QΓÖx=ζ'ϊ@SΒrqrv^ΚÛΦμΔÈîΑβÖÙBÊΧ/6ΟÀüz Β` , `KèΚZθYCL@U+â4XhΓ,ΧüννΚωódΩ*ΏLôΦΧθTΉïL1@rf!Ώ%v7Ïό@δŒΈ€VÎβEüẞ=yΏÛzωmÉcLΤΣWùzôßqΛτ Ç+rxü/ùμ3}Ι6KλZúÈgÀΑÂț`,alphabets);
+Decryption.DecryptRK(`set your ciphertext here` , `key goes here`,alphabets);
 //ZPÀΪwó(ΔΩSΪΓ€χψJαοuτțfήóΟϋÀ5nμ@)6óó6)@μn5ÀϋΟóήfțτuοαJψχ€ΓΪSΩΔ(ówΪÀPZ
 //ZÀw(ΩΪ€ψαuțήΟÀn@6ó)μ5ϋófτοJχΓSΔóΪP
 
-Decryption.DecryptSK(`ΓvΨξRΝΙξÂνΓgϊΞ/èΝÆL4hθæÆΎ!íΦΪ-ώ/Üψ/Χνț@χ{)!{ ÇN^gστπvνΒΔZΞ/ÂNËlΏιΫèș`, key1,key2,alphabets);
+Decryption.DecryptSK(`ciphertext here`, key1,key2,alphabets);
